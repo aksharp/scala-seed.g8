3be086db294c9d127a48cef54cf8d7bef3f6e277
Alexander Khotyanov <akhotyanov@tremorvideodsp.com>
2021-02-19 14:30:11 -0500

    property based testing for GreeterService

---
+++ src/main/g8/src/test/scala/$organization__packaged$/properties/GreeterServiceProps.scala
@@ -0,0 +1,69 @@
+package $organization$.properties
+
+import $organization$.config.AppConfig
+import com.tremorvideo.feature.flags.ExampleFeatureFlags
+import com.tremorvideo.lib.api.ObservableAndTraceable
+import com.tremorvideo.lib.api.fp.util.ObservableAndTraceableService
+import $organization$.services.GreeterServiceImpl
+import $organization$.test.util.TestUtils
+import $organization$.{GreetRequest, GreetResponse, NotWelcomeResponse, OutOfServiceResponse, WelcomeResponse}
+import monix.eval.Task
+import monix.execution.Scheduler
+import org.scalacheck.Prop.forAll
+import org.scalacheck.{Arbitrary, Gen, Properties}
+
+import scala.concurrent.duration.Duration
+
+object GreeterServiceProps extends Properties("GreeterServiceProps") with TestUtils {
+
+  import $organization$.mocks._  // code generated mocks, generators and todo: arbitraries
+
+  // TODO: auto generate in mocks
+  implicit val arbGreetRequest: Arbitrary[GreetRequest] =
+    Arbitrary(
+      GreetRequestGen()
+    )
+
+  // required implicits - can override with values as need be
+  implicit val observableAndTraceableService: ObservableAndTraceableService[Task] = anObservableAndTraceableService()
+  implicit val observableAndTraceable: ObservableAndTraceable = anObservableAndTraceable()
+  implicit val appConfig: AppConfig = anAppConfig()
+  implicit val scheduler: Scheduler = Scheduler.global
+
+  val greeterService = new GreeterServiceImpl
+
+  property(
+    """
+      INTENT: [GenerateGreetResponse]
+      FEATURE FLAGS: [ExampleFeatureFlags]
+      INPUT: [GreetRequest]
+      OUTPUT: [GreetResponse]
+        EITHER [WelcomeResponse] // happy path type
+        OR [NotWelcomeResponse]
+        OR [OutOfServiceResponse]
+      """.stripMargin
+  ) =
+    forAll {
+      (
+        exampleFeatureFlags: ExampleFeatureFlags,
+        input: GreetRequest
+      ) => {
+        ExampleFeatureFlags.set(exampleFeatureFlags)
+        val greetResponse: GreetResponse =
+          greeterService
+            .process(
+              greetRequest = input
+            )
+            .runSyncUnsafe(Duration.Inf)
+
+        greetResponse match {
+          case WelcomeResponse(_) => exampleFeatureFlags.enable && !exampleFeatureFlags.block.contains(input.name)
+          case NotWelcomeResponse(_) => exampleFeatureFlags.enable && exampleFeatureFlags.block.contains(input.name)
+          case OutOfServiceResponse(_) => !exampleFeatureFlags.enable
+          case _ => false
+        }
+      }
+
+    }
+
+}
--- src/main/g8/src/test/scala/$organization__packaged$/test.util/TestUtils.scala
+++ src/main/g8/src/test/scala/$organization__packaged$/test.util/TestUtils.scala
@@ -1,14 +1,66 @@
 package $organization$.test.util
 
+import cats.effect.Clock
 import $organization$.config.{AppConfig, ConsulDynamicConfig, HttpConfig}
-import com.tremorvideo.lib.api.{FeatureFlagsJson, ObservableAndTraceable}
+import com.tremorvideo.feature.flags.ExampleFeatureFlags
+import com.tremorvideo.lib.api.fp.util.{CorrelationIdGeneratorService, ObservableAndTraceableService}
+import com.tremorvideo.lib.api.{FeatureFlagsJson, ObservableAndTraceable, ObservableAndTraceableBase}
 import com.tremorvideo.lib.feature.flags.{Debug, DebugToConsole, DoNotObserveByDefault, Observe}
 import com.tremorvideo.lib.kafka.producer.TremorKafkaProducerConfig
 import com.tremorvideo.lib.metrics.{MetricsReporter, NoOp}
+import monix.eval.Task
+import org.joda.time.DateTime
+import org.scalacheck.{Arbitrary, Gen}
 
-import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.{ArrayBlockingQueue, TimeUnit}
+import scala.concurrent.duration.{FiniteDuration, TimeUnit}
 
 trait TestUtils {
+
+  val testAppName = $name$
+  val testClockTime: DateTime = DateTime.now()
+  val testCorrelationIdGeneratorService = new CorrelationIdGeneratorService[Task]
+
+  def aString() = Gen.alphaNumStr.sample.get
+
+  val testServiceInstanceCorrelationId: String = aString()
+  val testApiCallCorrelationId: String = aString()
+
+  def anObservableAndTraceable(
+                                serviceInstanceCorrelationId: String = testServiceInstanceCorrelationId,
+                                apiCallCorrelationId: String = testApiCallCorrelationId,
+                                currentDateTime: DateTime = testClockTime
+                              ): ObservableAndTraceable =
+    new ObservableAndTraceableBase(
+      serviceInstanceCorrelationId = serviceInstanceCorrelationId,
+      apiCallCorrelationId = apiCallCorrelationId,
+      apiCallTimestamp = currentDateTime.getMillis
+    )
+
+  val testClock = new Clock[Task] {
+    override def realTime(unit: TimeUnit): Task[Long] = Task.pure {
+      FiniteDuration(testClockTime.getMillis, TimeUnit.MILLISECONDS).toUnit(
+        unit
+      ).toLong
+    }
+
+    override def monotonic(unit: TimeUnit): Task[Long] =
+      Task.pure {
+        FiniteDuration(testClockTime.getMillis, TimeUnit.MILLISECONDS).toUnit(
+          unit
+        ).toLong
+      }
+  }
+
+  def anObservableAndTraceableService(
+                                       serviceName: String = testAppName,
+                                       clock: Clock[Task] = testClock,
+                                       correlationIdGeneratorService: CorrelationIdGeneratorService[Task] = testCorrelationIdGeneratorService
+                                     ) = new ObservableAndTraceableService[Task](
+    serviceName = serviceName,
+    clock = clock,
+    correlationIdGeneratorService = correlationIdGeneratorService
+  )
 
   def anObservableQueue(
                          capacity: Int = 100
@@ -28,7 +80,7 @@ trait TestUtils {
                      consulHostname = "consul.service.iad1.consul",
                      consulPort = 8500
                    ),
-                   appName: String = "$name$",
+                   appName: String = $name$,
                    dataCenter: String = "iad1",
                    debug: Debug = DebugToConsole,
                    observe: Observe = DoNotObserveByDefault,
@@ -55,5 +107,25 @@ trait TestUtils {
     debug = debug,
     observe = observe
   )
-  
+
+  def anExampleFeatureFlags(): ExampleFeatureFlags = genExampleFeatureFlags.sample.get
+
+  val genExampleFeatureFlags: Gen[ExampleFeatureFlags] =
+    for {
+      allow <- Gen.listOf(Gen.alphaNumStr)
+      block <- Gen.listOf(Gen.alphaNumStr)
+      enable <- Gen.oneOf(true, false)
+    } yield {
+      ExampleFeatureFlags(
+        allow = allow,
+        block = block,
+        enable = enable
+      )
+    }
+
+  implicit val arbExampleFeatureFlags: Arbitrary[ExampleFeatureFlags] =
+    Arbitrary(
+      genExampleFeatureFlags
+    )
+
 }
